f73a022b6ee9f20b4fe65562b8ef0109b2f11203
Maria Sevostyanova <letanina.maria@gmail.com>
2020-02-18 17:19:53 +0400

    first addition

---
+++ src/test/java/ru/netology/unitTEST/CashbackHackServiceTest.java
@@ -0,0 +1,34 @@
+package ru.netology.unitTEST;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void ShouldReturnCashbackIfAmountIs900() {
+
+       CashbackHackService cashbackHackService =  new CashbackHackService();
+
+       int amount = 900;
+
+       int actual = cashbackHackService.remain(amount);
+       int expected = 1000;
+
+       assertEquals(expected, actual);
+    }
+
+    @Test
+    void ShouldReturnCashbackIfAmountIs1000() {
+
+        CashbackHackService cashbackHackService =  new CashbackHackService();
+
+        int amount = 1000;
+
+        int actualResult = cashbackHackService.remain(amount);
+        int expectedResult = 1000;
+
+        assertEquals(expectedResult, actualResult);
+    }
+}
